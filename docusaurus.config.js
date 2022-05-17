1d13c840642c8c0297adebfe2ed4dc054b5b69dc
Robert Meyer <robert.meyer@ardonaghspecialty.com>
2022-05-17 13:45:41 +0100

    Changed icon to use SLOP instead of docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/slop.ico',
   organizationName: 'tronied', // Usually your GitHub org/user name.
   projectName: 'slop', // Usually your repo name.
 
