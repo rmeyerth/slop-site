c2479cf65d6703edb5904cc11197559cb2802716
Robert Meyer <tronied@yahoo.com>
2023-02-04 10:28:19 +0000

    Changed to use the load balancer

--- src/pages/try-it.js
+++ src/pages/try-it.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '@theme/Layout';
 
-const addRecordEndpoint = "https://ec2-13-42-62-216.eu-west-2.compute.amazonaws.com:443/api/v1/request";
+const addRecordEndpoint = "https://slop-load-3718735.eu-west-2.elb.amazonaws.com:443/api/v1/request";
 
 const addNewRecord = async (Title, Author) => {
   const RecordBodyParameters = {
